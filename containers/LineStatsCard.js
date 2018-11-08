d2964ff50632993245c253bd15ea8683e29a9b9b
Grégoire Fora <gfora@okina.fr>
2018-11-08 14:34:20 +0100

    Updated bogu version (0.6.7 -> 0.6.8) and num valid days

--- containers/LineStatsCard.js
+++ containers/LineStatsCard.js
@@ -126,7 +126,7 @@ class LineStatsCard extends React.Component {
                   </div>
                   <div style={validDateStartStyle}>{stats.startDate}</div>
                   <div style={validDateMiddleStyle}>
-                    {stats.validFromDate} (90 jours)
+                    {stats.validFromDate} (30 jours)
                   </div>
                   <div style={validDateEndStyle}>{stats.endDate}</div>
                 </div>
